5588a66c522e7992fad0d64e0637b0dd89cb7d4b
Calista-H <calista.x.huang@gmail.com>
2019-12-08 16:27:16 -0800

    The better version

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 import java.util.*;
-
-public class Hello_World {
+public class HelloWorld {
    public static void main(String[] args) {
-
-    System.out.printlin("Hello World!"\n);
+     System.out.println("Bonjour mes amis! Il est le meilleur program!");
+     System.out.println("Translation: Hell my friends! This is the better program :)");
+   }
 } 
